13e0678c8702cd216abfd0cfae5cbb7950862767
oliveirarogerio <oliveirarogerio35@gmail.com>
2022-08-02 21:29:50 -0400

    create modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
- export function App() {
+Modal.setAppElement('#root')
+
+export function App() {
+  //repasse de funções
+
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
-      <Dashboard/>
-      <GlobalStyle/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastras Transação</h2>
+      </Modal>
+
+      <GlobalStyle />
     </>
   );
 }
- 
